d8d4e7829721787a5ac2b9c7dff515064f0a54da
Ivan Shulgin <multizookie@gmail.com>
2023-07-15 23:29:52 +0700

    test: linter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom"
 import { Application } from "./Application"
 import { initStore } from "./store"
 
